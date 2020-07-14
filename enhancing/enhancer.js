248d4e2d6976d258ad542bdd449c618bc62678ff
Matthew Taylor <majikpig@gmail.com>
2020-07-13 21:31:07 -0400

    fail and tests for get

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -9,7 +9,22 @@ function succeed(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  let newItem = { ...item };
+  if (item.enhancement<15) {
+    newItem.durability -= 5;
+    if (newItem.durability<0) {
+      newItem.durability = 0;
+    }
+  } else {
+    newItem.durability -= 10;
+    if (newItem.durability<0) {
+      newItem.durability = 0;
+    }
+    if (item.enhancement>16) {
+      newItem.enhancement--;
+    }
+  }
+  return newItem;
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -38,8 +38,11 @@ describe("item enhancer unit tests", ()=>{
     expect(enhancer.repair(test_items[4]).durability).toBe(100);
   });
   it("repairs without modifying item", ()=> {
-    expect(enhancer.repair(test_items[0]).durability).toBe(100);
-    expect(test_items[0].durability).toBe(10);
+    const oldItem = test_items[0];
+    const newItem = enhancer.repair(oldItem);
+    expect(newItem.durability).toBe(100);
+    expect(oldItem.durability).toBe(10);
+    expect(oldItem).toEqual(test_items[0]);
   });
   it("succeeds on enhancement", ()=>{
     let newItem = enhancer.succeed(test_items[0]);
@@ -57,5 +60,44 @@ describe("item enhancer unit tests", ()=>{
     const newItem = enhancer.succeed(oldItem);
     expect(newItem.enhancement).toBe(1);
     expect(oldItem.enhancement).toBe(0);
+    expect(oldItem).toEqual(test_items[0]);
+  });
+  it("fails on enhancement", ()=> {
+    let newItem = enhancer.fail(test_items[0]);
+    expect(newItem.enhancement).toBe(0);
+    expect(newItem.durability).toBe(5);
+    newItem = enhancer.fail(test_items[1]);
+    expect(newItem.enhancement).toBe(10);
+    expect(newItem.durability).toBe(45);
+    newItem = enhancer.fail(test_items[2]);
+    expect(newItem.enhancement).toBe(15);
+    expect(newItem.durability).toBe(65);
+    newItem = enhancer.fail(test_items[3]);
+    expect(newItem.enhancement).toBe(16);
+    expect(newItem.durability).toBe(70);
+    newItem = enhancer.fail(test_items[4]);
+    expect(newItem.enhancement).toBe(19);
+    expect(newItem.durability).toBe(90);
+  });
+  it("fails without modifying item", ()=> {
+    const oldItem = test_items[4];
+    const newItem = enhancer.fail(oldItem);
+    expect(newItem.enhancement).toBe(19);
+    expect(oldItem.enhancement).toBe(20);
+    expect(newItem.durability).toBe(90);
+    expect(oldItem.durability).toBe(100);
+    expect(oldItem).toEqual(test_items[4]);
+  });
+  it("gets a name altered according to the rules", ()=> {
+    let newItem = enhancer.get(test_items[0]);
+    expect(newItem.name).toBe("Low Item");
+    newItem = enhancer.get(test_items[1]);
+    expect(newItem.name).toBe("[+10] Mid Item");
+    newItem = enhancer.get(test_items[2]);
+    expect(newItem.name).toBe("[+15] Mid-High Item");
+    newItem = enhancer.get(test_items[3]);
+    expect(newItem.name).toBe("[+17] Mid-High Item 2");
+    newItem = enhancer.get(test_items[4]);
+    expect(newItem.name).toBe("[+20] High item");
   });
 });
